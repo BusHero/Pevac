25d42aadbd20968b91099c486328ebe80d605e87
Cervac Petru <petru.cervac@gmail.com>
2021-09-19 19:42:58 +0300

    Refactored Parser.Tokens.cs file
    Moved all the optional tokens to a separate file called Parser.Tokens.Optional.cs

--- Pevac/Parser/Parser.Tokens.cs
+++ Pevac/Parser/Parser.Tokens.cs
@@ -6,132 +6,75 @@ namespace Pevac
 {
     public static partial class Parser
     {
-        private static Parser<Void>? optionalBoolToken;
         private static Parser<Void>? startObjectToken;
         private static Parser<Void>? booleanToken;
-        private static Parser<Void>? endObjectToken ;
+        private static Parser<Void>? endObjectToken;
+        private static Parser<Void>? nullToken;
+        private static Parser<Void>? falseToken;
+        private static Parser<Void>? trueToken;
+        private static Parser<Void>? numberToken;
+        private static Parser<Void>? stringToken;
+        private static Parser<Void>? propertyNameToken;
+        private static Parser<Void>? endArrayToken;
+        private static Parser<Void>? startArrayToken;
 
         /// <summary>
         /// Parser for <see cref="JsonTokenType.StartObject"/> token.
         /// </summary>
         public static Parser<Void> StartObjectToken => startObjectToken ??= ParseToken(JsonTokenType.StartObject);
-
-        //public static Result<Void> StartObjectToken(ref Utf8JsonReader reader, JsonSerializerOptions? options)
-        //{
-        //    return ParseToken(ref reader, options, JsonTokenType.StartObject);
-        //}
-
+        
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.EndObject"/> token.
         /// </summary>
         public static Parser<Void> EndObjectToken => endObjectToken ??= ParseToken(JsonTokenType.EndObject);
+
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.StartArray"/> token.
         /// </summary>
-        public static Parser<Void> StartArrayToken { get; } = ParseToken(JsonTokenType.StartArray);
+        public static Parser<Void> StartArrayToken => startArrayToken ??= ParseToken(JsonTokenType.StartArray);
 
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.EndArray"/> token.
         /// </summary>
-        public static Parser<Void> EndArrayToken { get; } = ParseToken(JsonTokenType.EndArray);
+        public static Parser<Void> EndArrayToken => endArrayToken ??= ParseToken(JsonTokenType.EndArray);
 
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.PropertyName"/> token.
         /// </summary>
-        public static Parser<Void> PropertyNameToken { get; } = ParseToken(JsonTokenType.PropertyName);
+        public static Parser<Void> PropertyNameToken => propertyNameToken ??= ParseToken(JsonTokenType.PropertyName);
 
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.String"/> token.
         /// </summary>
-        public static Parser<Void> StringToken { get; } = ParseToken(JsonTokenType.String);
-
-        /// <summary>
-        /// Parser for the <see cref="JsonTokenType.String"/> 
-        /// or <see cref="JsonTokenType.None"/> token.
-        /// </summary>
-        public static Parser<Void> OptionalStringToken { get; } = ParseToken(JsonTokenType.String, JsonTokenType.Null);
-
+        public static Parser<Void> StringToken => stringToken ??= ParseToken(JsonTokenType.String);
+       
+  
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.Number"/> token.
         /// </summary>
-        public static Parser<Void> NumberToken { get; } = ParseToken(JsonTokenType.Number);
-
-        /// <summary>
-        /// Parser for the <see cref="JsonTokenType.Number"/> 
-        /// or <see cref="JsonTokenType.Null"/> token.
-        /// </summary>
-        public static Parser<Void> OptionalNumber { get; } = ParseToken(JsonTokenType.Number, JsonTokenType.Null);
-
+        public static Parser<Void> NumberToken => numberToken ??= ParseToken(JsonTokenType.Number);
+    
+     
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.True"/> token.
         /// </summary>
-        public static Result<Void> TrueToken(ref Utf8JsonReader reader, JsonSerializerOptions? options = default)
-        {
-            return ParseToken(ref reader, options, JsonTokenType.True);
-        }
-
-        /// <summary>
-        /// Parser for the <see cref="JsonTokenType.True"/> 
-        /// or <see cref="JsonTokenType.Null"/> tokens.
-        /// </summary>
-        public static Parser<Void> OptionalTrueToken { get; } = ParseToken(JsonTokenType.True, JsonTokenType.Null);
-
+        public static Parser<Void> TrueToken => trueToken ??= ParseToken(JsonTokenType.True);
+     
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.False"/> token.
         /// </summary>
-        public static Parser<Void> FalseToken { get; } = ParseToken(JsonTokenType.False);
+        public static Parser<Void> FalseToken => falseToken ??= ParseToken(JsonTokenType.False);
 
-        /// <summary>
-        /// Parser for the <see cref="JsonTokenType.False"/> 
-        /// or <see cref="JsonTokenType.Null"/> tokens.
-        /// </summary>
-        public static Parser<Void> OptionalFalseToken { get; } = ParseToken(JsonTokenType.False, JsonTokenType.Null);
-
-        
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.True"/> 
         /// or <see cref="JsonTokenType.False"/> tokens.
         /// </summary>
         public static Parser<Void> BooleanToken => booleanToken ??= ParseToken(JsonTokenType.True, JsonTokenType.False);
 
-        /// <summary>
-        /// Parser for the <see cref="JsonTokenType.True"/> 
-        /// or <see cref="JsonTokenType.False"/> 
-        /// or <see cref="JsonTokenType.Null"/> tokens.
-        /// </summary>
-        public static Parser<Void> OptionalBoolToken => optionalBoolToken ??= ParseToken(JsonTokenType.True, JsonTokenType.False, JsonTokenType.Null);
-
-        //= ParseToken(JsonTokenType.True, JsonTokenType.False, JsonTokenType.Null);
-
 
         /// <summary>
         /// Parser for the <see cref="JsonTokenType.Null"/> token.
         /// </summary>
-        public static Parser<Void> NullToken { get; } = ParseToken(JsonTokenType.Null);
-
-        /// <summary>
-        /// Parses a token.
-        /// </summary>
-        /// <param name="reader"></param>
-        /// <param name="options"></param>
-        /// <param name="tokens"></param>
-        /// <returns></returns>
-        public static Result<Void> ParseToken(
-            ref Utf8JsonReader reader,
-            JsonSerializerOptions? options,
-            params JsonTokenType[] tokens)
-        {
-            return reader.Read() switch
-            {
-                false => Result
-                .Failure<Void>("Cannot read the next token. You've probably reached the end of stream"),
-                true when tokens.Contains(reader.TokenType) => Result
-                .Success(Void.Default),
-                _ => Result
-                .Failure<Void>(""),
-            };
-        }
-
-
+        public static Parser<Void> NullToken => nullToken ??= ParseToken(JsonTokenType.Null);
     }
 }
