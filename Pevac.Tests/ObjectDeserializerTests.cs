f5a1337ec79013e56f79537661ed3822ef9c2d40
Cervac Petru <petru.cervac@gmail.com>
2021-09-20 19:45:21 +0300

    First iteration of ParseObject
    Finished the first iteration of the ParseObject method. I don't like this one too much :( but it's a start

---
+++ Pevac.Tests/ObjectDeserializerTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using FluentAssertions;
+using System.Text;
+using System.Text.Json;
+using System;
+
+namespace Pevac.Tests
+{
+    public class ObjectDeserializerTests
+    {
+        private record Data(string Foo, string Bar);
+
+        [Fact]
+        public void DeserializeObject()
+        {
+            var json = @"
+            {
+                ""foo"": ""foo"",
+                ""bar"": ""bar""
+            }";
+
+            var bytes = Encoding.UTF8.GetBytes(json);
+            var reader = new Utf8JsonReader(bytes);
+
+            Parser<Data> parser = Parser.ParseObject(propertyName => propertyName switch
+            {
+                "foo" => from value in Parser.String
+                         select new Func<Data, Data>(data => data with { Foo = value }),
+                "bar" => from value in Parser.String
+                         select new Func<Data, Data>(data => data with { Bar = value }),
+                _ => Parser.Failure<Func<Data, Data>>()
+            }).Select(updater => updater(new Data("", "")));
+
+            Parser.Parse(parser, ref reader, default).Should().Be(new Data("foo", "bar"));
+        }
+    }
+}
---
+++ Pevac/Parser/Parser.Object.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pevac
+{
+    public static partial class Parser
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Parser<Func<T, T>?> ParseObject<T>(Func<string, Parser<Func<T, T>>> parserSelector)
+        {
+            return parserSelector switch
+            {
+                null => throw new ArgumentNullException(nameof(parserSelector)),
+                not null => 
+                    from _ in StartObjectToken
+                from updaters in (from propertyName in PropertyName
+#pragma warning disable CS8619 // Nullability of reference types in value doesn't match target type.
+                                              from updater in parserSelector(propertyName)
+#pragma warning restore CS8619 // Nullability of reference types in value doesn't match target type.
+                                              select updater)
+                                              .Many()
+                            from __ in EndObjectToken
+                            select new Func<T, T>(t => updaters.Aggregate(t, (data, updater) => updater(data)))
+            }
+            ;
+        }
+    }
+}
